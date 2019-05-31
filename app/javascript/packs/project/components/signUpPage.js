f6874744e5ba46dfecf3892ef0b91fcee3bbcf53
Hang Yuan <hyuan13@illinois.edu>
2019-05-31 10:42:20 -0500

    modify signuppage

--- app/javascript/packs/project/components/signUpPage.js
+++ app/javascript/packs/project/components/signUpPage.js
@@ -6,6 +6,7 @@ import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } 
 class Signup extends React.Component{
 
   state = {
+
     type: 'user',
     name: null,
     username: null,
@@ -15,12 +16,15 @@ class Signup extends React.Component{
   }
 
   constructor(props){
+
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+
   }
 
   handleChange(e){
+
     if(e.target.id === 'name')this.setState({name:e.target.value});
     if(e.target.id === 'email')this.setState({username:e.target.value});
     if(e.target.id === 'password')this.setState({password:e.target.value});
@@ -28,7 +32,6 @@ class Signup extends React.Component{
       var button1 = document.getElementById('user');
       var button2 = document.getElementById('designer')
       var button = e.target.id==='user'? 0:1
-      console.log(button1);
       if(button == 0){
         this.setState({type:'user'})
         button1.className = 'btn btn-block Ripple-parent btn-lg btn-purple z-depth-1 white-text'
@@ -43,144 +46,131 @@ class Signup extends React.Component{
       var form = document.getElementById('form');
       form.style.visibility = 'visible';
     }
+
   }
 
   handleSubmit(e){
 
     fetch('/myaccount', {
       method: 'POST',
-      body: JSON.stringify({name:this.state.name, username:this.state.username, password:this.state.password}),
-      headers:{
+      body: JSON.stringify({name: this.state.name, username: this.state.username, password: this.state.password}),
+      headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials':true,
           'Access-Control-Allow-Methods':'POST, GET',
           "Content-Type": "application/json"
-        }
+      }
     }).then(res => res.json())
-      .then(data=>{
-            alert(data)});
+      .then(data => {alert(data)});
+
   }
 
 	render(){
+
 		return(
 			<div>
 				<Header></Header>
 
-			<MDBContainer style={{position: 'relative', top:'100px', left:"20px", width:'100%'}}>
-      <MDBRow className='justify-content-center'>
+			  <MDBContainer style = {{position: 'relative', top:'100px', left:"20px", width:'100%'}}>
+          <MDBRow className = 'justify-content-center'>
+          <MDBCol md = "5">
+          <p className = "text-center mb-4 mt-0" fontSize = '15' style = {{fontSize:'30px'}}> I want a new account </p>
+          <div style = {{height:'100px'}}>
+            <MDBRow>
+            <MDBCol md = '6'>
+            <MDBBtn id = 'user'
+                    color = 'purple'
+                    type = "button"
+                    outline
+                    onClick = {this.handleChange}
+                    className = "btn-block z-depth-1 white-text">
+              User
+            </MDBBtn>
+            </MDBCol>
+            <MDBCol md ='6'>
+            <MDBBtn id = 'designer'
+                    color = 'purple'
+                    type = "button"
+                    outline
+                    onClick = {this.handleChange}
+                    className = "btn-block z-depth-1 white-text">
+              Designer
+            </MDBBtn>
+            </MDBCol>
+            </MDBRow>
+          </div>
 
-
-
-        <MDBCol md = "5">
-        <p className="text-center mb-4 mt-0" fontSize='15' style={{fontSize:'30px'}}> I want a new account
-        </p>
-        <div style = {{height:'100px'}}>
-        <MDBRow>
-        <MDBCol md = '6'>
-        <MDBBtn   id = 'user'
-                  color = 'purple'
-                  type="button"
-                  outline
-                  onClick={this.handleChange}
-                  className="btn-block z-depth-1 white-text"
-
-                >
-                  User
-                </MDBBtn>
-        </MDBCol>
-        <MDBCol md ='6'>
-        <MDBBtn   id = 'designer'
-                  color = 'purple'
-                  type="button"
-                  outline
-                  onClick={this.handleChange}
-                  className="btn-block z-depth-1 white-text"
-                >
-                  Designer
-                </MDBBtn>
-        </MDBCol>
-        </MDBRow>
-        </div>
-        <div id = "form" style = {{visibility: "hidden"}}>
-
-        <form onSubmit={(event) => this.handleSubmit(event)}>
-            <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
-              Your name
-            </label>
-            <input
-              id='name'
-              type="text"
-              onChange={this.handleChange}
-              className="form-control"
-            />
-            <br />
-            <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
-              Your email
-            </label>
-            <input
-              id='email'
-              type="email"
-              onChange={this.handleChange}
-              className="form-control"
-            />
-            <br />
-            <label
-              htmlFor="defaultFormRegisterConfirmEx"
-              className="grey-text"
-            >
-              Confirm your email
-            </label>
-            <input
-              id = 'confirm'
-              type="email"
-              id="defaultFormRegisterConfirmEx"
-              className="form-control"
-            />
-            <br />
-            <label
-              htmlFor="defaultFormRegisterPasswordEx"
-              className="grey-text"
-            >
-              Your password
-            </label>
-            <input
-              id = 'password'
-              type="password"
-              onChange={this.handleChange}
-              className="form-control"
-            />
-            <div className="text-center mb-4 mt-5">
+          <div id = "form" style = {{visibility: "hidden"}}>
+            <form onSubmit = {(event) => this.handleSubmit(event)}>
+              <label htmlFor = "defaultFormRegisterNameEx" className = "grey-text">
+                Your name
+              </label>
+              <input
+                id = 'name'
+                type = "text"
+                onChange = {this.handleChange}
+                className = "form-control"/>
+              <br />
+              <label htmlFor = "defaultFormRegisterEmailEx" className = "grey-text">
+                Your email
+              </label>
+              <input
+                id = 'email'
+                type = "email"
+                onChange = {this.handleChange}
+                className = "form-control"/>
+              <br />
+              <label
+                htmlFor = "defaultFormRegisterConfirmEx"
+                className = "grey-text">
+                Confirm your email
+              </label>
+              <input
+                id = 'confirm'
+                type = "email"
+                id = "defaultFormRegisterConfirmEx"
+                className = "form-control"/>
+              <br />
+              <label
+                htmlFor = "defaultFormRegisterPasswordEx"
+                className = "grey-text">
+                Your password
+              </label>
+              <input
+                id = 'password'
+                type = "password"
+                onChange = {this.handleChange}
+                className = "form-control"/>
+              <div className = "text-center mb-4 mt-5">
                 <MDBBtn
                   color = 'purple'
-                  type="submit"
-                  className="btn-block z-depth-2 white-text"
-                >
+                  type = "submit"
+                  className = "btn-block z-depth-2 white-text">
                   Create Account
                 </MDBBtn>
-            </div>
-        </form>
+              </div>
+            </form>
 
             <p className="font-small grey-text d-flex justify-content-center">
-                Already have an account?
-                <a
-                  href="/login"
-                  className="dark-grey-text font-weight-bold ml-1"
-                >
-                  Log in
-                </a>
+              Already have an account?
+            <a
+              href="/login"
+              className="dark-grey-text font-weight-bold ml-1">
+              Log in
+            </a>
             </p>
-        </div>
-         </MDBCol>
-      </MDBRow>
-    </MDBContainer>	
+
+          </div>
+          </MDBCol>
+          </MDBRow>
+        </MDBContainer>	
 			
+			  <div style={{position: 'relative', top:'300px', width:"100%"}}>
+    		  <Footer></Footer>
+    	  </div>
+    	</div>
 
-
-			<div style={{position: 'relative', top:'300px', width:"100%"}}>
-    			<Footer></Footer>
-    		</div>
-    		</div>
-			)
+		)
 	}
 }
 export default Signup
