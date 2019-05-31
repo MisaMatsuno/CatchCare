e50fe05e0567f9d0f49164e626d0567cd14ad3aa
Hang Yuan <hyuan13@illinois.edu>
2019-05-31 15:10:55 -0500

    add message and status

--- app/controllers/myaccount_controller.rb
+++ app/controllers/myaccount_controller.rb
@@ -17,10 +17,10 @@ class MyaccountController < ApplicationController
 	def create
 		@myaccount = Myaccount.new(account_params)
 		if Myaccount.where(:username => @myaccount.username).exists?
-			render json: {"state": "This email address has been registed"}
+			render json: {"state": "FAIL", "message": "This email address has been registered!"}
 		else
 			if @myaccount.save
-				render json: {"state": "You have successfully created a account!"}
+				render json: {"state": "OK", "message": "You have successfully created an account!"}
 			else
 				render json: @myaccount.errors, status: :unprocessable_entity
 			end
