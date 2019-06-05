8ebf757c98b09fd97fff6ce5ca72ab26f9507420
Hang Yuan <hyuan13@illinois.edu>
2019-06-05 16:29:25 -0500

    ...

--- app/javascript/packs/project/components/comps/Header.js
+++ app/javascript/packs/project/components/comps/Header.js
@@ -20,83 +20,94 @@ class Header extends React.Component {
 
   }
 
-  componentDidMount(){
+  componentDidMount() {
+
     var search = document.getElementById('search');
-    if(this.props.location.pathname==='/'){
-      search.style.visibility='hidden';
+    if(this.props.location.pathname==='/') {
+      search.style.visibility='hidden'
     }
     var cart_count = localStorage.cart;
-    if (cart_count!=null && cart_count!=undefined && cart_count!='0'){
-      this.setState({cart:"Cart ("+cart_count+")"});
+    if(cart_count!=null && cart_count!=undefined && cart_count!='0') {
+      this.setState({cart:"Cart ("+cart_count+")"})
     }
-    else{
-      this.setState({cart:"Cart"});
+    else {
+      this.setState({cart:"Cart"})
     }
+
   }
 
 	state = {
-  	isOpen: false
+
+    isOpen: false
+    
 	};
 
   handleClick(e){
+
     console.log(localStorage)
-    if(localStorage.user==null || localStorage.user==undefined ||localStorage.user==''){
-      this.props.history.push('/login');
+    if(localStorage.user == null || localStorage.user == undefined || localStorage.user == '') {
+      this.props.history.push('/login')
     }
-    else{
-      this.props.history.push('/myaccount/'+localStorage.user+'/personal');
-      }
+    else {
+      this.props.history.push('/myaccount/' + localStorage.user + '/personal')
+    }
+
   }
 
   handleSubmit(e) {
-    e.preventDefault();
+
+    e.preventDefault()
     this.props.history.push('/solution') 
-}
 
-	toggleCollapse = () =>{
-		this.setState({isOpen:!this.state.isOpen});
-	}
+  }
+
+	toggleCollapse = () => {
+
+    this.setState({isOpen:!this.state.isOpen});
+    
+  }
+  
   render() {
+
     return(
-
-    	<div className="fixed-top">
-     <MDBNavbar color="purple" dark expand="md">
-        <MDBNavbarBrand>
-          <strong className="white-text">Catch Globe</strong>
-        </MDBNavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse} />
-        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-          <MDBNavbarNav left>
-            <MDBNavItem active>
-              <MDBNavLink to="/">Home</MDBNavLink>
-            </MDBNavItem>
+    	<div className = "fixed-top">
+        <MDBNavbar color = "purple" dark expand = "md">
+          <MDBNavbarBrand>
+            <strong className = "white-text"> Catch Care </strong>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick = {this.toggleCollapse} />
+          <MDBCollapse id = "navbarCollapse3" isOpen = {this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem active>
+                <MDBNavLink to = "/"> Home </MDBNavLink>
+              </MDBNavItem>
             
-            <MDBNavItem>
-              <MDBNavLink to="/forum">Forum</MDBNavLink>
-            </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to = "/forum"> Forum </MDBNavLink>
+              </MDBNavItem>
 
-            <MDBNavItem id = 'search' style={{visibility:"visible"}}>
-              <MDBFormInline waves>
-                <div className="md-form my-0">
-                  <input className="form-control mr-sm-2" type="text" onSubmit={this.handleSubmit} placeholder="Search" aria-label="Search" />
-                </div>
-              </MDBFormInline>
-            </MDBNavItem>
+              <MDBNavItem id = 'search' style = {{visibility: "visible"}}>
+                <MDBFormInline waves>
+                  <div className = "md-form my-0">
+                    <input className = "form-control mr-sm-2" type = "text" onSubmit = {this.handleSubmit} placeholder = "Search" aria-label = "Search" />
+                  </div>
+                </MDBFormInline>
+              </MDBNavItem>
             
-          </MDBNavbarNav>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <MDBNavLink onClick={this.handleClick} to="#!">My Account</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="/cart">{this.state.cart}</MDBNavLink>
-            </MDBNavItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBNavbar>
-      </div>
-      
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBNavLink onClick = {this.handleClick} to = "#!"> My Account </MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to = "/cart"> {this.state.cart} </MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </div>  
     )
+
   }
 }
 export default withRouter(Header);
