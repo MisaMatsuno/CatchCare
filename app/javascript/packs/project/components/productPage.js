52f5fa4b06cb1d4f0926ff74ae3736cacb200c89
Hang Yuan <hyuan13@illinois.edu>
2019-06-17 15:54:31 -0500

    finish solution product tab

--- app/javascript/packs/project/components/productPage.js
+++ app/javascript/packs/project/components/productPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import StarRatingComponent from 'react-star-rating-component';
 import Header from './comps/Header'
 import Footer from './comps/Footer'
 import { MDBBtn, MDBBtnGroup, MDBIcon, MDBCol, MDBRow } from 'mdbreact';
@@ -119,17 +121,17 @@ class Product extends React.Component {
     return(
       <div className = 'App'>
         <Header></Header>
-        <MDBContainer fluid style={{position: 'relative', top:'100px', left:'40px', width:'100%'}}> 
+        <MDBContainer fluid style = {{position: 'relative', top:'100px', left:'40px', width:'100%'}}> 
           <MDBRow>
             <MDBCol style = {{position:'relative', top:'50px'}} col = '4'>
     	        <div className = 'col-md-8 mb-3'>
-                <img src={this.state.img} className='img-fluid z-depth-1' alt='Responsive image'/>
+                <img src = {this.state.img} className = 'img-fluid z-depth-1' alt = 'Responsive image'/>
               </div>
-              <div className='row'>
+              <div className = 'row'>
                 {this.state.image && this.state.image.map((image, index) => {
                   return (
-                    <div className='col-lg-2 col-md-2 col-sm-2 mb-2 waves-effect hoverable'>
-                      <img onClick ={(event) => imageClick(event)} src={ image } className='img-fluid z-depth-1' alt='Responsive image'/>
+                    <div className = 'col-lg-2 col-md-2 col-sm-2 mb-2 waves-effect hoverable'>
+                      <img onClick = {(event) => imageClick(event)} src = { image } className = 'img-fluid z-depth-1' alt = 'Responsive image'/>
                     </div>
                   );
                 })}
@@ -140,66 +142,42 @@ class Product extends React.Component {
               <div className = 'col-lg-8 col-md-8 col-sm-12'>
                 <p className = 'text-center mb-2 mt-0 font-weight-bold' fontSize = '15' style = {{fontSize:'30px'}}> { this.state.name } </p>
                 <MDBListGroup>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>
-                    <span className='float-left' style = {{fontSize:'40px'}}>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>
+                    <span className = 'float-left' style = {{fontSize:'40px'}}>
                       price: 
                     </span>
-                    <span className='float-left font-weight-bold' style = {{fontSize:'40px', color:'purple'}}>
+                    <span className = 'float-left font-weight-bold' style = {{fontSize:'40px', color:'purple'}}>
                     <strong> {this.state.price}$ </strong>
                     </span>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Shipping From
-                    <span className='float-left font-weight-bold' style = {{fontSize:'20px', color:'purple'}}>
-                      Beijing
-                    </span>
-                  </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Shipping To
-                    <span className='float-left font-weight-bold' style = {{fontSize:'20px', color:'purple'}}>
-                      United States
-                    </span>
-                  </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Rating
-                    <ul className='list-group list-group-horizontal list-unstyled'>
-                      <li>
-                        <MDBIcon icon='star' />
-                      </li>
-                      <li>
-                        <MDBIcon icon='star' />
-                      </li>
-                      <li>
-                        <MDBIcon icon='star' />
-                      </li>
-                      <li>
-                        <MDBIcon icon='star' />
-                      </li>
-                      <li>
-                        <MDBIcon far icon='star' />
-                      </li>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Rating
+                    <ul className = 'list-group list-group-horizontal list-unstyled'>
+                      <StarRatingComponent name = 'rate' starCount = {5} value = {this.state.rating}/>
                     </ul>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Manufacturer
-                    <span className='float-left' style = {{fontSize:'20px', color:'purple'}}>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Manufacturer
+                    <span className = 'float-left' style = {{fontSize:'20px', color:'purple'}}>
                       { this.state.manufacturer }
                     </span>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Monthly Sales
-                    <span className='float-left' style = {{fontSize:'20px', color:'purple'}}>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Monthly Sales
+                    <span className = 'float-left' style = {{fontSize:'20px', color:'purple'}}>
                       { this.state.monthly_sales }
                     </span>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Reviews
-                    <span className='float-left' style = {{fontSize:'20px', color:'purple'}}>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Reviews
+                    <span className = 'float-left' style = {{fontSize:'20px', color:'purple'}}>
                       { this.state.review }
                     </span>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>In Stock
-                    <span className='float-left' style = {{fontSize:'20px', color:'purple'}}>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>In Stock
+                    <span className = 'float-left' style = {{fontSize:'20px', color:'purple'}}>
                       { this.state.in_stock }
                     </span>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Category
-                    <select style= {{width:'50%'}} className='browser-default custom-select' value={this.state.select_category} 
-                    onChange={this.handleChange} id='select_category'>
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Category
+                    <select style = {{width:'50%'}} className = 'browser-default custom-select' value = {this.state.select_category} 
+                    onChange = {this.handleChange} id = 'select_category'>
                       <option>Choose your option</option>
                       {this.state.category && this.state.category.map((category, index) => {
                         return (
@@ -208,19 +186,19 @@ class Product extends React.Component {
                       })}
                     </select>
                   </MDBListGroupItem>
-                  <MDBListGroupItem className=' d-flex justify-content-between align-items-center' hover>Quantity 
-                    <input placeholder = '0' style ={{width:'15%'}} className='form-control' id='quantity' onChange={this.handleChange} 
-                    type='number' min='0' value={this.state.quantity}/>                               
+                  <MDBListGroupItem className = 'd-flex justify-content-between align-items-center' hover>Quantity 
+                    <input placeholder = '0' style = {{width:'15%'}} className = 'form-control' id = 'quantity' onChange={this.handleChange} 
+                    type = 'number' min = '0' value = {this.state.quantity}/>                               
                   </MDBListGroupItem>
                 </MDBListGroup>
                 <MDBRow>
-                  <MDBCol md = '6' className='d-flex justify-content-center'>
-                    <MDBBtn id = 'cart' onClick={this.handleClick} className='white-text' color='purple'>
+                  <MDBCol md = '6' className = 'd-flex justify-content-center'>
+                    <MDBBtn id = 'cart' onClick = {this.handleClick} className = 'white-text' color = 'purple'>
                       Add to Cart
                     </MDBBtn>
                   </MDBCol>
-                  <MDBCol md = '6' className='d-flex justify-content-center'>
-                    <MDBBtn className='white-text' color='purple'>
+                  <MDBCol md = '6' className = 'd-flex justify-content-center'>
+                    <MDBBtn className = 'white-text' color = 'purple'>
                       Buy Now
                     </MDBBtn>
                   </MDBCol>
@@ -231,14 +209,14 @@ class Product extends React.Component {
         </MDBContainer>
 
         <div style = {{position:'relative', top:'100px', left:'50px'}}>
-          <Tabs defaultActiveKey='detail' id='detail-tab'>
-            <Tab eventKey='detail' title='Product Details'>
+          <Tabs defaultActiveKey = 'detail' id = 'detail-tab'>
+            <Tab eventKey = 'detail' title = 'Product Details'>
             </Tab>
-            <Tab eventKey='review' title='Reviews'>
+            <Tab eventKey = 'review' title = 'Reviews'>
             </Tab>
           </Tabs>
         </div>
-    	  <div style={{position: 'relative', top:'300px', width:'100%'}}>
+    	  <div style = {{position: 'relative', top:'300px', width:'100%'}}>
     	    <Footer></Footer>
     	  </div>
 	    </div>
--- app/javascript/packs/project/components/solutionPage.js
+++ app/javascript/packs/project/components/solutionPage.js
@@ -1,36 +1,46 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import StarRatingComponent from 'react-star-rating-component';
 import Header from './comps/Header'
 import Footer from './comps/Footer'
 import Search from './comps/Search'
 import queryString from 'query-string'
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardFooter, MDBIcon, MDBTooltip,  MDBBadge, MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardFooter, MDBIcon, MDBTooltip,  MDBBadge, MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBBtn } from 'mdbreact';
 import {Tabs, Tab, TabContainer, TabContent, TabPane } from 'react-bootstrap'
 
 
 class Solution extends React.Component{
 
   state = {
-    activeItemPills: "1",
+
+    activeItemPills: '1',
     products: []
+
   }
 
   constructor(props){
+
     super(props)
     this.handleClick = this.handleClick.bind(this)
+
   }
 
   handleClick(e){
-    this.prop.history.push("/products/1")
+
+    this.prop.history.push('/products/1')
+
   }
 
 
-togglePills = tab => () => {
-  if (this.state.activePills !== tab) {
-  this.setState({
-    activeItemPills: tab
-    });
+  togglePills = tab => () => {
+
+    if (this.state.activePills !== tab) {
+      this.setState({
+        activeItemPills: tab
+      });
+    }
+
   }
-}
 
 
   componentDidMount() {
@@ -42,7 +52,7 @@ togglePills = tab => () => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials':true,
         'Access-Control-Allow-Methods':'POST, GET',
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
     }).then(res => 
         res.json())
@@ -61,230 +71,78 @@ togglePills = tab => () => {
             tmp.push(product)
           }
           this.setState({products: tmp});
-          console.log(this.state)
+          //console.log(this.state)
       })
     
   }
 
 	render(){
 
-
 		return(
 			<div>
 				<Header></Header>
-        <MDBContainer style={{position:"relative", top:'200px'}}>
-        <p> Search Result for {queryString.parse(this.props.location.search).queryKey} :</p>
-        
-        <Tabs defaultActiveKey="product" id="solution-tab">
-          <Tab eventKey="product" title="Products">
-          <MDBRow>
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard cascade narrow ecommerce>
-            <a href="/products/1">
-            <MDBCardImage
-              cascade
-              src="https://img.alicdn.com/imgextra/i1/509554376/O1CN01zXorDP1iCFgoaomIz_!!509554376.jpg_430x430q90.jpg"
-              top
-              alt="sample photo"
-              overlay="white-slight"
-              className = 'img-fluid'
-            /></a>
-            <MDBCardBody cascade className="text-center">
-              <a href="/products/1" className="grey-text">
-                <h5>Crutch</h5>
-              </a>
-              <MDBCardTitle>
-                <strong>
-                  <a href="/products/1/detail">Outshine Double Crutch</a>
-                </strong>
-              </MDBCardTitle>
-              <ul className="rating list-group list-unstyled justify-content-center list-group-horizontal">
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon far icon="star" />
-                </li>
-              </ul>
-              <MDBCardText>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
-              </MDBCardText>
-              <MDBCardFooter className="px-1">
-                <span className="float-left font-weight-bold">
-                  <strong>49$</strong>
-                </span>
-              </MDBCardFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard cascade narrow ecommerce>
-            <MDBCardImage
-              cascade
-              src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/2.jpg"
-              top
-              alt="sample photo"
-              overlay="white-slight"
-            />
-            <MDBCardBody cascade className="text-center">
-              <a href="#!" className="grey-text">
-                <h5>Shoes</h5>
-              </a>
-              <MDBCardTitle>
-                <strong>
-                  <a href="#!">High heels</a>
-                </strong>
-              </MDBCardTitle>
-              <ul className="rating list-group list-unstyled justify-content-center list-group-horizontal">
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-              </ul>
-              <MDBCardText>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
-              </MDBCardText>
-              <MDBCardFooter className="px-1">
-                <span className="float-left font-weight-bold">
-                  <strong>89$</strong>
-                </span>
-              
-              </MDBCardFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard cascade narrow ecommerce>
-            <MDBCardImage
-              cascade
-              src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/4.jpg"
-              top
-              alt="sample photo"
-              overlay="white-slight"
-            />
-            <MDBCardBody cascade className="text-center">
-              <a href="#!" className="grey-text">
-                <h5>Outwear</h5>
-              </a>
-              <MDBCardTitle>
-                <strong>
-                  <a href="#!">Brown coat</a>
-                </strong>
-              </MDBCardTitle>
-              <ul className="rating list-group list-unstyled justify-content-center list-group-horizontal">
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon far icon="star-half" />
-                </li>
-              </ul>
-              <MDBCardText>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
-              </MDBCardText>
-              <MDBCardFooter className="px-1">
-                <span className="float-left font-weight-bold">
-                  <strong>59$</strong>
-                </span>
-
-              </MDBCardFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-
-
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard cascade narrow ecommerce>
-            <MDBCardImage
-              cascade
-              src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/3.jpg"
-              top
-              alt="sample photo"
-              overlay="white-slight"
-            />
-            <MDBCardBody cascade className="text-center">
-              <a href="#!" className="grey-text">
-                <h5>Blouses</h5>
-              </a>
-              <MDBCardTitle>
-                <strong>
-                  <a href="#!">Shirt</a>
-                </strong>
-              </MDBCardTitle>
-              <ul className="rating list-group list-unstyled justify-content-center list-group-horizontal">
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon icon="star" />
-                </li>
-                <li>
-                  <MDBIcon far icon="star" />
-                </li>
-              </ul>
-              <MDBCardText>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
-              </MDBCardText>
-              <MDBCardFooter className="px-1">
-                <span className="float-left font-weight-bold">
-                  <strong>119$</strong>
-                </span>
-              </MDBCardFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-
-      </MDBRow>
-          </Tab>
-          <Tab eventKey="tips" title="Tips">
-            <Search></Search>
-          </Tab>
-          <Tab eventKey="designs" title="Designs">
+        <MDBContainer style = {{position:'relative', top:'200px'}}>
+          <p> Search Result for { queryString.parse(this.props.location.search).queryKey } :</p>
+          <Tabs defaultActiveKey = 'product' id = 'solution-tab'>
+            <Tab eventKey = 'product' title = 'Products'>
+            <MDBRow>
+              {this.state.products && this.state.products.map((product, index) => {
+                const photo = product.image.split(' ')[0]
+                const id = product.id
+                return (
+                  <MDBCol lg = '5' md = '5' className = 'mb-lg-0 mb-4'>
+                    <MDBCard cascade narrow ecommerce>
+                      <a href = {'/products/' + product.id + '/detail'}>
+                        <MDBCardImage
+                          cascade
+                          src = { photo }
+                          top
+                          alt = 'sample photo'
+                          overlay = 'white-slight'
+                          className = 'img-fluid'
+                        />
+                      </a>
+                      <MDBCardBody cascade className = 'text-center'>
+                        <a href = {'/products/' + product.id + '/detail'} className = 'grey-text'>
+                          <h5>{ product.kind }</h5>
+                        </a>
+                        <MDBCardTitle>
+                          <strong>
+                            <a href = {'/products/' + product.id + '/detail'}>{ product.name }</a>
+                          </strong>
+                        </MDBCardTitle>
+                        <ul className = 'rating list-group list-unstyled justify-content-center list-group-horizontal'>
+                          <StarRatingComponent name = 'rate' starCount = {5} value = {product.rating}/>
+                        </ul>
+                        <MDBCardText>
+                          { product.detail }
+                        </MDBCardText>
+                        <MDBCardFooter className = 'px-1'>
+                          <span className = 'float-left font-weight-bold'>
+                            <strong>{ product.price }$</strong>
+                         </span>
+                        </MDBCardFooter>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                );
+              })}
+            </MDBRow>
+            </Tab>
+            <Tab eventKey = 'tips' title = 'Tips'>
               <Search></Search>
-          </Tab>
-        </Tabs>
-       
-	     </MDBContainer>
-			<div style={{position: 'relative', top:'500px', width:"100%"}}>
+            </Tab>
+            <Tab eventKey = 'designs' title = 'Designs'>
+              <Search></Search>
+            </Tab>
+          </Tabs>
+	      </MDBContainer>
+			  <div style = {{position: 'relative', top:'500px', width:'100%'}}>
     			<Footer></Footer>
     		</div>
-    		</div>
-			)
+    	</div>
+    )
+    
 	}
 }
 export default Solution
